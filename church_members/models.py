93f61df0a143987b22e68d943573a5a687ec39d3
Robert Muchinga <muchingarobertjr@gmail.com>
2026-05-30 14:42:44 +0200

    created church_member app and member model finalized

---
+++ church_members/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Member(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    first_name = models.CharField(max_length=50)
+
+    last_name = models.CharField(max_length=50)
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES
+    )
+
+    date_of_birth = models.DateField()
+
+    date_of_baptism = models.DateField()
+
+    phone_number = models.CharField(max_length=20)
+
+    email = models.EmailField(
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    address = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
